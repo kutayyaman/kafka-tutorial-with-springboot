5a5655526a7597a23072e372102658d17371291d
kutayyaman <yamankutay1@gmail.com>
2022-09-06 21:10:02 +0300

    initial commit

---
+++ src/test/java/com/kutayyaman/springbootkafkatutorial/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutayyaman.springbootkafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
