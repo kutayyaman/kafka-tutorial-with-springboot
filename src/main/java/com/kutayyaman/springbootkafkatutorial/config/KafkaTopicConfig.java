2c7864a554bf958660c5768a3e3815e6598e11db
kutayyaman <yamankutay1@gmail.com>
2022-09-06 23:15:29 +0300

    KafkaTopicConfig is refactored and summary of what I did is I extracted the name of example topic name as a public static final field

--- src/main/java/com/kutayyaman/springbootkafkatutorial/config/KafkaTopicConfig.java
+++ src/main/java/com/kutayyaman/springbootkafkatutorial/config/KafkaTopicConfig.java
@@ -7,10 +7,11 @@ import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
+    public static final String exampleTopicName = "example";
 
     @Bean
     public NewTopic exampleTopic() {
-        return TopicBuilder.name("example")
+        return TopicBuilder.name(exampleTopicName)
                 .build();
     }
 }
