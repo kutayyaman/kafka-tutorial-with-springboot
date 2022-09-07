209a167424eb3dd7f63bc5aeaf7fa2f04285a13f
kutayyaman <yamankutay1@gmail.com>
2022-09-07 22:13:37 +0300

    a consumer is created

---
+++ src/main/java/com/kutayyaman/springbootkafkatutorial/kafka/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.kutayyaman.springbootkafkatutorial.kafka;
+
+import com.kutayyaman.springbootkafkatutorial.config.KafkaTopicConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = KafkaTopicConfig.EXAMPLE_TOPIC_NAME, groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(String message) {
+        LOGGER.info(String.format("Message received from topic:%s -> message:%s ",
+                KafkaTopicConfig.EXAMPLE_TOPIC_NAME,
+                message));
+
+    }
+}
