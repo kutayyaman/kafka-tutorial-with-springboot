efadfef691457aa99827b7760a9fc3eb27abad41
kutayyaman <yamankutay1@gmail.com>
2022-09-07 23:40:33 +0300

    a new topic created with the name "json_topic"

--- src/main/java/com/kutayyaman/springbootkafkatutorial/config/KafkaTopicConfig.java
+++ src/main/java/com/kutayyaman/springbootkafkatutorial/config/KafkaTopicConfig.java
@@ -7,11 +7,18 @@ import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
-    public static final String STRING_TOPIC = "stringTopic";
+    public static final String STRING_TOPIC = "string_topic";
+    public static final String JSON_TOPIC = "json_topic";
 
     @Bean
     public NewTopic exampleTopic() {
         return TopicBuilder.name(STRING_TOPIC)
+                .build();
+    }
+
+    @Bean
+    public NewTopic jsonTopic() {
+        return TopicBuilder.name(JSON_TOPIC)
                 .build();
     }
 }
